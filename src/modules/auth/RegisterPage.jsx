d110db26f286fa2d2a3fafa6bfe4e2f5170231e1
Mayur <mayurbhuva505@gmail.com>
2023-10-14 07:23:23 +0530

    changes for notifications

--- src/modules/auth/RegisterPage.jsx
+++ src/modules/auth/RegisterPage.jsx
@@ -18,6 +18,7 @@ import messaging from '@react-native-firebase/messaging';
 import RNCallKeep from 'react-native-callkeep'
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
+import PushNotification from "react-native-push-notification";
 const engine = createAgoraRtcEngine();
 
 const RegisterPage = () => {
@@ -27,6 +28,67 @@ const RegisterPage = () => {
     const [error, setError] = useState()
     const { setUser, user } = useContext(UserParamContext)
     const navigation = useNavigation()
+
+
+
+    PushNotification.configure({
+        // (optional) Called when Token is generated (iOS and Android)
+        onRegister: function (token) {
+            console.log("TOKEN:", token);
+        },
+
+        // (required) Called when a remote is received or opened, or local notification is opened
+        onNotification: function (notification) {
+            console.log("NOTIFICATION:", notification);
+            PushNotification.localNotification({
+                channelId: notification?.channelId,
+                smallIcon: "",
+                soundName: notification?.sound,
+                // color: "red",
+                title: notification?.title,
+                // bigText: notification?.body, // it will be shown when user expands notification
+                message: notification?.message, // (required)
+            });
+            // process the notification
+
+            // (required) Called when a remote is received or opened, or local notification is opened
+            // notification.finish(PushNotificationIOS.FetchResult.NoData);
+        },
+
+        // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+        onAction: function (notification) {
+            console.log("ACTION:", notification.action);
+            console.log("NOTIFICATION:", notification);
+
+            // process the action
+        },
+
+        // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+        onRegistrationError: function (err) {
+            console.error(err.message, err);
+        },
+
+        // IOS ONLY (optional): default: all - Permissions to register.
+        permissions: {
+            alert: true,
+            badge: true,
+            sound: true,
+        },
+
+        // Should the initial notification be popped automatically
+        // default: true
+        popInitialNotification: true,
+
+        /**
+         * (optional) default: true
+         * - Specified if permissions (ios) and token (android and ios) will requested or not,
+         * - if not, you must call PushNotificationsHandler.requestPermissions() later
+         * - if you are not using remote notification or do not have Firebase installed, use this:
+         *     requestPermissions: Platform.OS === 'ios'
+         */
+        requestPermissions: true,
+    });
+
 
     useEffect(() => {
         getToken()
@@ -127,8 +189,16 @@ const RegisterPage = () => {
             console.log("🚀 ~ file: IncominCall.jsx:40 ~ IncominCall ~ callUUID:", callUUID)
             const handle = 'recipient_username'; // The recipient's username or phone number
             // RNCallKeep.answerIncomingCall(callUUID)
-
-            RNCallKeep.displayIncomingCall(callUUID, handle, 'Incoming Call', 'default', true);
+            // PushNotification.localNotification({
+            //     channelId: noti?.data?.android_channel_id,
+            //     smallIcon: "",
+            //     soundName: noti?.data?.sound,
+            //     // color: "red",
+            //     title: noti?.notification?.title,
+            //     // bigText: noti?.notification?.body, // it will be shown when user expands notification
+            //     message: noti?.notification?.body, // (required)
+            // });
+            // RNCallKeep.displayIncomingCall(callUUID, handle, 'Incoming Call', 'default', true);
             //   PushNotification.localNotification({
             //     channelId: noti?.data?.android_channel_id,
             //     smallIcon: '',
